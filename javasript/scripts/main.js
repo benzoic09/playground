c6b7fc53179b3632894d83463b9a3c8f83cf4482
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-01-09 12:40:52 +0300

    testing Javascript

--- javasript/scripts/main.js
+++ javasript/scripts/main.js
@@ -1,2 +1,11 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+const button_A = document.querySelector("#button_A");
+const heading_A = document.querySelector("#heading_A");
+
+button_A.onclick = () => {
+  const name = prompt("What is your name?");
+  alert(`Hello ${name}, nice to see you!`);
+  heading_A.textContent = `Welcome ${name}`;
+}; 
